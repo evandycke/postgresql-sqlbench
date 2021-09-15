a0bb219ac9d4704538a83d337e9adea8984d106f
Elie <elie.vandycke@elosi.com>
2021-09-15 09:14:41 +0200

    Create qualified.sql

---
+++ scripts/qualified.sql
@@ -0,0 +1,2 @@
+SELECT businessentityid, nationalidnumber, loginid, jobtitle, birthdate, maritalstatus, gender, hiredate, salariedflag, vacationhours, sickleavehours, currentflag, rowguid, modifieddate, organizationnode
+	FROM humanresources.employee;
