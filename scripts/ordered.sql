c3496e320eb42e72447cdc029550f3ebac28c44a
Elie <elie.vandycke@elosi.com>
2021-09-15 09:14:46 +0200

    Create ordered.sql

---
+++ scripts/ordered.sql
@@ -0,0 +1,3 @@
+SELECT businessentityid, nationalidnumber, loginid, jobtitle, birthdate, maritalstatus, gender, hiredate, salariedflag, vacationhours, sickleavehours, currentflag, rowguid, modifieddate, organizationnode
+	FROM humanresources.employee
+	ORDER BY loginid;
