a5abfebc4bb5033b0128bd21cc8fc6986c86ebf0
Elie <evandycke@gmail.com>
2023-03-17 09:07:41 +0100

    Init repo

--- scripts/ordered.sql
+++
@@ -1,3 +0,0 @@
-SELECT businessentityid, nationalidnumber, loginid, jobtitle, birthdate, maritalstatus, gender, hiredate, salariedflag, vacationhours, sickleavehours, currentflag, rowguid, modifieddate, organizationnode
-	FROM humanresources.employee
-	ORDER BY loginid;
